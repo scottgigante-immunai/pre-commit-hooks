c4eeb0948727677e72c1178a9991321330f74630
Scott Gigante <scott.gigante@immunai.com>
2022-02-08 15:56:27 -0500

    check directories too

--- pre_commit_hooks/check_case_insensitive_paths.py
+++ pre_commit_hooks/check_case_insensitive_paths.py
@@ -2,26 +2,41 @@ import argparse
 import os
 import pathlib
 import subprocess
+from functools import lru_cache
 from typing import Optional
 from typing import Sequence
 
 
-def list_files(filename: str) -> Sequence[str]:
+def git_base_dir(filename: str) -> str:
     with subprocess.Popen(
         ['git', 'rev-parse', '--show-toplevel'],
         stdout=subprocess.PIPE,
         cwd=os.path.dirname(filename),
     ) as process:
         stdout, _ = process.communicate()
-    git_dir = pathlib.Path(stdout.decode().strip()).resolve()
-    return [dir.as_posix() for dir in git_dir.iterdir()]
+    return os.path.abspath(stdout.decode().strip())
 
 
-def check_file(filename: str, file_list: Sequence[str]) -> Optional[str]:
+def list_files(base_dir: str) -> Sequence[str]:
+    return tuple(dir.as_posix() for dir in pathlib.Path(base_dir).iterdir())
+
+
+@lru_cache(None)
+def check_file(
+    filename: str,
+    file_list: Sequence[str],
+    base_dir: str,
+) -> Optional[str]:
+    assert filename.startswith(base_dir)
     for other_file in file_list:
+        # exact file match
         if other_file != filename and other_file.lower() == filename.lower():
             return other_file
-    return None
+    # base case
+    if filename == base_dir:
+        return None
+    # recurse
+    return check_file(os.path.dirname(filename), file_list, base_dir)
 
 
 def main(argv: Optional[Sequence[str]] = None) -> int:
@@ -30,13 +45,14 @@ def main(argv: Optional[Sequence[str]] = None) -> int:
     args = parser.parse_args(argv)
     filenames = [os.path.abspath(filename) for filename in args.filenames]
 
-    file_list = list_files(filenames[0])
+    base_dir = git_base_dir(filenames[0])
+    file_list = list_files(base_dir)
     retval = 0
     for filename in filenames:
-        match = check_file(filename, file_list)
+        match = check_file(filename, file_list, base_dir)
         if match:
             print(
-                f'file {filename} matches file {match} '
+                f'file {filename} conflicts with file {match} '
                 'in case-insensitive filesystems',
             )
             retval = 1
